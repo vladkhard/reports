04a2beb799d29e87b490ee31ffd94d091580e4fc
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-11-07 14:13:28 +0200

    Fixed default formatter

--- reports/log.py
+++ reports/log.py
@@ -1,15 +1,21 @@
 import logging
-from logging.config import fileConfig, dictConfig
 from ExtendedJournalHandler import ExtendedJournalHandler
+
 
 SYS_LOG = {
     "SYSLOG_IDENTIFIER": "API_BILLING",
     "level": logging.INFO
 }
 
+
 def getLogger(name):
     logger = logging.getLogger(name)
     handler = ExtendedJournalHandler(**SYS_LOG)
-    handler.setFormatter(logger.root.handlers[0].formatter)
+    if logger.root.handlers:
+        handler.setFormatter(logger.root.handlers[0].formatter)
+    else:
+        handler.setFormatter(
+            logging.Formatter("%(asctime)s %(name)-8s %(message)s")
+        )
     logger.addHandler(handler)
     return logger
