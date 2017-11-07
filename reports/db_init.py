96f0cc48952b88d5ff8d2ac0e9bca5c036ad3453
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-11-07 14:10:44 +0200

    Extend logging in db initialization script

--- reports/db_init.py
+++ reports/db_init.py
@@ -1,14 +1,17 @@
-from couchdb import Server, Session
-from couchdb.http import Unauthorized, extract_credentials
-from reports.helpers import create_db_url
 import argparse
-import logging
+from couchdb import Server, Session
 from ConfigParser import ConfigParser
 from pbkdf2 import PBKDF2
+from logging.config import fileConfig
 
-LOGGER = logging.getLogger(__name__)
+from reports.helpers import create_db_url
+from reports.log import getLogger
 
-SECURITY = {u'admins': {u'names': [], u'roles': ['_admin']}, u'members': {u'names': [], u'roles': ['_admin']}}
+
+SECURITY = {
+    u'admins': {u'names': [], u'roles': ['_admin']},
+    u'members': {u'names': [], u'roles': ['_admin']}
+}
 VALIDATE_DOC_ID = '_design/_auth'
 VALIDATE_DOC_UPDATE = """function(newDoc, oldDoc, userCtx){
     if(newDoc._deleted && newDoc.tenderID) {
@@ -26,19 +29,28 @@ VALIDATE_DOC_UPDATE = """function(newDoc, oldDoc, userCtx){
 
 
 def couchdb_connection(config):
+
+    LOGGER = getLogger("BILLING")
+    LOGGER.info("Start database initialization")
+
     # CouchDB connection
     db_name = config.get('db', 'name')
     db_host = config.get('db', 'host')
     db_port = config.get('db', 'port')
     admin_name = config.get('admin', 'username')
     admin_pass = config.get('admin', 'password')
-    aserver = Server(create_db_url(db_host, db_port, admin_name, admin_pass), session=Session(retry_delays=range(10)))
+    aserver = Server(
+        create_db_url(db_host, db_port, admin_name, admin_pass),
+        session=Session(retry_delays=range(10)))
     users_db = aserver['_users']
 
     username = config.get('user', 'username')
     password = config.get('user', 'password')
 
-    user_doc = users_db.get('org.couchdb.user:{}'.format(username), {'_id': 'org.couchdb.user:{}'.format(username)})
+    user_doc = users_db.get(
+        'org.couchdb.user:{}'.format(username),
+        {'_id': 'org.couchdb.user:{}'.format(username)}
+    )
 
     if not user_doc.get('derived_key', '') or PBKDF2(password, user_doc.get('salt', ''), user_doc.get('iterations', 10)).hexread(int(len(user_doc.get('derived_key', '')) / 2)) != user_doc.get('derived_key', ''):
         user_doc.update({
@@ -47,7 +59,10 @@ def couchdb_connection(config):
             "type": "user",
             "password": password
         })
-        LOGGER.info("Updating api db main user", extra={'MESSAGE_ID': 'update_api_main_user'})
+        LOGGER.info(
+            "Updating api db main user",
+            extra={'MESSAGE_ID': 'update_api_main_user'}
+        )
         users_db.save(user_doc)
     if db_name not in aserver:
         aserver.create(db_name)
@@ -55,13 +70,19 @@ def couchdb_connection(config):
 
     SECURITY[u'members'][u'names'] = [username, ]
     if SECURITY != db.security:
-        LOGGER.info("Updating api db security", extra={'MESSAGE_ID': 'update_api_security'})
+        LOGGER.info(
+            "Updating api db security",
+            extra={'MESSAGE_ID': 'update_api_security'}
+        )
         db.security = SECURITY
 
     auth_doc = db.get(VALIDATE_DOC_ID, {'_id': VALIDATE_DOC_ID})
     if auth_doc.get('validate_doc_update') != VALIDATE_DOC_UPDATE % username:
         auth_doc['validate_doc_update'] = VALIDATE_DOC_UPDATE % username
-        LOGGER.info("Updating api db validate doc", extra={'MESSAGE_ID': 'update_api_validate_doc'})
+        LOGGER.info(
+            "Updating api db validate doc",
+            extra={'MESSAGE_ID': 'update_api_validate_doc'}
+        )
         db.save(auth_doc)
 
 
@@ -72,15 +93,10 @@ def run():
 
     config = ConfigParser()
     config.read(args.c)
+    fileConfig(args.c)
 
     couchdb_connection(config)
 
 
 if __name__ == '__main__':
     run()
-
-
-
-
-
-    
