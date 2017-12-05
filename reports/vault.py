9602ba8596a221492d3cbacb3f9f5f7a2dfdc8ce
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-12-05 15:04:32 +0200

    Added vault.py to VCS control

---
+++ reports/vault.py
@@ -0,0 +1,29 @@
+import hvac
+import os
+
+
+class Config(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.vault_addr = os.environ.get('VAULT_ADDR') or \
+            self.config.get('vault', 'address')
+        self.token = os.environ.get('VAULT_TOKEN') or \
+            self.config.get('vault', 'token')
+        self.base = self.config.get('brokers_keys', 'path')
+
+
+class Vault(object):
+
+    def __init__(self, config):
+        self.config = Config(config)
+        self.client = hvac.Client(
+            url=self.config.vault_addr,
+            token=self.config.token
+            )
+
+    def get(self, key, default=None):
+        data = self.client.read(key).get('data')
+        if data:
+            return data
+        return default
